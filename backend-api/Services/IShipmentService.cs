0a3a54b11ba2ffca2f1ed00ad73167aaf549d419
Motje <Mohamaadflaha2014@gmail.com>
2025-05-24 23:39:53 +0200

    ishipment service updated

--- backend-api/Services/IShipmentService.cs
+++ backend-api/Services/IShipmentService.cs
@@ -1,4 +1,5 @@
 using backend_api.Models;
+using backend_api.DTOs;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,8 +12,7 @@ namespace backend_api.Services
         Task<Shipment?> UpdateStatusAsync(int id, string newStatus, string updatedBy);
         Task<IEnumerable<Shipment>> GetShipmentsForUserAsync(string username, DateTime date);
         Task<IEnumerable<Shipment>> GetShipmentsForUserAsync(string username);
-
-
+        Task<Shipment> CreateShipmentAsync(CreateShipmentDto shipmentDto);
 
 
     }
