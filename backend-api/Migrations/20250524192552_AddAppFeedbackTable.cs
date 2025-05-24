9cbb7b75c3f4f3a8ea79616a1edf1d09e45ecbff
Motje <Mohamaadflaha2014@gmail.com>
2025-05-24 21:28:26 +0200

    new migrations run, now in database

---
+++ backend-api/Migrations/20250524192552_AddAppFeedbackTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace backend_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAppFeedbackTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppFeedbacks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OverallRating = table.Column<int>(type: "integer", nullable: false),
+                    BestFeature = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    MissingFeature = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    Suggestions = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: true),
+                    WouldRecommend = table.Column<bool>(type: "boolean", nullable: true),
+                    SubmittedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppFeedbacks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppFeedbacks");
+        }
+    }
+}
