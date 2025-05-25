ec6db7e85c66ed433096b53c117a7d93698baa5c
Motje <Mohamaadflaha2014@gmail.com>
2025-05-25 21:04:53 +0200

    feat: remove unnecessary line in back button implementation in ShipmentDetails

--- app/shipment/shipmentdetails.tsx
+++ app/shipment/shipmentdetails.tsx
@@ -278,7 +278,6 @@ const ShipmentDetails = () => {
         {/* Optional Back Button */}
         <View style={{ position: "absolute", top: hp(4), left: wp(5) }}>
           <TouchableOpacity onPress={() => router.replace("/(tabs)/scan")}>
-
             <Ionicons name="arrow-back" size={wp(8)} color="#fff" />
           </TouchableOpacity>
         </View>
