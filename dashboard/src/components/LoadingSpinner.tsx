b090b536300f6d1ccb0cfce2f7337117daf12a88
herkam3 <1078244@hr.nl>
2025-06-17 12:10:30 +0200

    Component for loading pages. (Animated)

---
+++ dashboard/src/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+const LoadingSpinner = () => (
+  <div className="flex items-center justify-center w-full h-full py-8">
+    <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-yellow-400 border-solid border-opacity-60"></div>
+  </div>
+);
+
+export default LoadingSpinner;
